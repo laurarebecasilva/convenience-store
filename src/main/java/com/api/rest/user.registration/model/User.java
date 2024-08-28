936fd4fe25408f904bc64a8afb906530c1db7ccd
Laura Rebeca <laura.rebeca.d.silva@accenture.com>
2024-08-28 11:47:12 -0300

    feat: add updateData method in User class

--- src/main/java/com/api/rest/user.registration/model/User.java
+++ src/main/java/com/api/rest/user.registration/model/User.java
@@ -1,7 +1,9 @@
 package com.api.rest.user.registration.model;
 
 import com.api.rest.user.registration.dto.UserDto;
+import com.api.rest.user.registration.dto.UserUpdateDto;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -23,6 +25,7 @@ public class User {
     private String username;
 
     @Column(unique = true)
+    @Pattern(regexp = "\\d{8}")
     private String password;
 
     @Column(unique = true)
@@ -32,5 +35,14 @@ public class User {
         this.username = data.username();
         this.password = data.password();
         this.email = data.email();
+    }
+
+    public void updateData(UserUpdateDto userUpdateDto) {
+        if (userUpdateDto.username() != null) {
+            this.username = userUpdateDto.username(); //se o retorno do put vier preenchido (nao nulo), username pode ser atualizado.
+        }
+        if (userUpdateDto.password() != null) {
+            this.password = userUpdateDto.password();
+        }
     }
 }
