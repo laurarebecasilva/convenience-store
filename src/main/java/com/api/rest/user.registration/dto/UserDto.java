7959af1c6c1b24c8f5fd2bfdcf7f1afa60c961af
Laura Rebeca <laura.rebeca.d.silva@accenture.com>
2024-09-13 21:19:19 -0300

    feat: add validations for username, password, and email attributes

--- src/main/java/com/api/rest/user.registration/dto/UserDto.java
+++ src/main/java/com/api/rest/user.registration/dto/UserDto.java
@@ -1,23 +1,26 @@
 package com.api.rest.user.registration.dto;
 
+
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record UserDto(
         @Id
         Long id,
 
-        @NotBlank
+        @NotBlank(message = "Username cannot be blank")
+        @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
         String username,
 
-        @NotBlank
-        @Pattern(regexp = "\\d{6,8}", message = "The password must contain between 6 and 8 digits") //Expressão regular
+        @NotBlank(message = "Password cannot be blank")
+        @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z]).{8,}$", message = "Password must be at least 8 characters long and include both letters and numbers")
         String password,
 
-        @NotBlank
-        @Email
-        String email)
-{
+        @NotBlank(message = "Email cannot be blank")
+        @Email(message = "Email must be valid")
+        String email) {
+
 }
