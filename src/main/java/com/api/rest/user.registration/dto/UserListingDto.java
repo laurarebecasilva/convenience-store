4f20b438ea7cb764ee02c023114be881d968b4cb
Laura Rebeca <laura.rebeca.d.silva@accenture.com>
2024-09-13 21:20:37 -0300

    feat: add status attribute to UserListingDTO

--- src/main/java/com/api/rest/user.registration/dto/UserListingDto.java
+++ src/main/java/com/api/rest/user.registration/dto/UserListingDto.java
@@ -1,11 +1,12 @@
 package com.api.rest.user.registration.dto;
 
+import com.api.rest.user.registration.model.Status;
 import com.api.rest.user.registration.model.User;
 
-public record UserListingDto(Long id, String username, String email) {
+public record UserListingDto(Long id, String username, String email, Status status) {
 
     public UserListingDto(User user) { //construtor
-        this(user.getId(), user.getUsername(), user.getEmail());
+        this(user.getId(), user.getUsername(), user.getEmail(), user.getStatus());
     }
 }
 //Com a classe UserListingDto, consigo fitrar os dados que quero exibir,
