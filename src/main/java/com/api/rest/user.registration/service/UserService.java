972d556fed0cdd70bd15fd9d836fae42af19a6e4
Laura Rebeca <laura.rebeca.d.silva@accenture.com>
2024-09-13 21:23:55 -0300

    refactor: update listUsers method to list only active users and add statusUserInactive method

--- src/main/java/com/api/rest/user.registration/service/UserService.java
+++ src/main/java/com/api/rest/user.registration/service/UserService.java
@@ -3,6 +3,7 @@ package com.api.rest.user.registration.service;
 import com.api.rest.user.registration.dto.UserDto;
 import com.api.rest.user.registration.dto.UserListingDto;
 import com.api.rest.user.registration.dto.UserUpdateDto;
+import com.api.rest.user.registration.model.Status;
 import com.api.rest.user.registration.model.User;
 import com.api.rest.user.registration.repository.UserRepository;
 import jakarta.transaction.Transactional;
@@ -11,7 +12,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Service //Serviço spring
+@Service //Serviço spring - gestão durante a execução.
 public class UserService {
 
     @Autowired
@@ -24,7 +25,7 @@ public class UserService {
     }
 
     public List<UserListingDto> listUsers() {
-        return userRepository.findAll()
+        return userRepository.findByStatus(Status.ACTIVE)
                 .stream()
                 .map(UserListingDto::new)
                 .collect(Collectors.toList()); //retorna a lista de usuários que foi convertida por meio do stream, agora lista os dados sem comprometer os dados sensiveis.
@@ -41,5 +42,12 @@ public class UserService {
     public void deleteUser(Long id) {
         User user = userRepository.getReferenceById(id);
         userRepository.delete(user);
+    }
+
+    @Transactional
+    public void statusUserInactive(Long id, Status status) {
+        User user = userRepository.getReferenceById(id);
+        user.setStatus(status.INACTIVE);
+        userRepository.save(user);
     }
 }
