f412858b5b233c4002be688e1e65f54a003bb59e
Laura Rebeca <laura.rebeca.d.silva@accenture.com>
2024-09-13 21:25:52 -0300

    feat: add validation constraints to UserUpdateDTO attributes

--- src/main/java/com/api/rest/user.registration/dto/UserUpdateDto.java
+++ src/main/java/com/api/rest/user.registration/dto/UserUpdateDto.java
@@ -1,15 +1,22 @@
 package com.api.rest.user.registration.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record UserUpdateDto(
-        @NotBlank
+        @NotBlank(message = "Username cannot be blank")
+        @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
         String username,
 
-        @NotBlank
-        @Pattern(regexp = "\\d{8}")
-        String password) {
-    }
+        @NotBlank(message = "Password cannot be blank")
+        @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z]).{8,}$", message = "Password must be at least 8 characters long and include both letters and numbers")
+        String password,
+
+        @NotBlank(message = "Email cannot be blank")
+        @Email(message = "Email must be valid")
+        String email) {
+}
 
 
