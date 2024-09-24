e5d3d0f9ee19f19d83787856e7f99ebd179cd399
Laura Rebeca <laura.rebeca.d.silva@accenture.com>
2024-09-24 18:05:48 -0300

    refactor(application): rename application run class

--- src/main/java/com/api/rest/conveniencestore/ConvenienceStoreApplication.java
+++ src/main/java/com/api/rest/conveniencestore/ConvenienceStoreApplication.java
@@ -1,13 +1,13 @@
-package com.api.rest.user.registration;
+package com.api.rest.conveniencestore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserRegistrationApplication {
+public class ConvenienceStoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserRegistrationApplication.class, args);
+		SpringApplication.run(ConvenienceStoreApplication.class, args);
 		System.out.println("Project started");
 	}
 }
