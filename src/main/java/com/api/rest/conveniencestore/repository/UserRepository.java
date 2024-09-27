b24b2e3164076d3be2cc13d0428e5d5a3220f6f7
Laura Rebeca <laura.rebeca.d.silva@accenture.com>
2024-09-27 19:01:03 -0300

    feat: add findByUsername method in UserRepository

--- src/main/java/com/api/rest/conveniencestore/repository/UserRepository.java
+++ src/main/java/com/api/rest/conveniencestore/repository/UserRepository.java
@@ -3,12 +3,16 @@ package com.api.rest.conveniencestore.repository;
 import com.api.rest.conveniencestore.model.User;
 import com.api.rest.conveniencestore.enums.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.Optional;
+
 //Esta classe está herdando do JpaRepository o método FindAll, então os métodos do crud,
 // ja existe declarado na interface, por meio do JPA.
 public interface UserRepository extends JpaRepository <User, Long> { //repository acessa o banco de dados.
-
     //busca os usuarios com base no statos ativo ou inativo
     Collection<User> findByStatus(Status status);
+
+    Optional<UserDetails> findByUsername(String username);
 }
