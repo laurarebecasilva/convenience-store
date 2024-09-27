8bb00c1214b6b166d981336579e09e1370be5bd3
Laura Rebeca <laura.rebeca.d.silva@accenture.com>
2024-09-27 19:02:32 -0300

    chore: change enum to string in migrations

--- src/main/resources/db/migration/V2__create-table-products.sql
+++ src/main/resources/db/migration/V2__create-table-products.sql
@@ -4,5 +4,6 @@ CREATE TABLE products (
     category VARCHAR(50) NOT NULL,
     price DECIMAL(10, 2) NOT NULL,
     stock_quantity INT NOT NULL,
-    expiration_date DATE
+    expiration_date DATE,
+    status VARCHAR(20) NOT NULL DEFAULT 'REGISTERED'
 );
