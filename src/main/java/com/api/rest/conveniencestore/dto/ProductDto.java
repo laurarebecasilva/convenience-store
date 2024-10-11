41889ed47e921ca8ef3f02111fdbc8686aa52d99
Laura Rebeca <laura.rebeca.d.silva@accenture.com>
2024-10-11 14:14:45 -0300

    feat: add column validation for name and stockQuantity

--- src/main/java/com/api/rest/conveniencestore/dto/ProductDto.java
+++ src/main/java/com/api/rest/conveniencestore/dto/ProductDto.java
@@ -1,6 +1,7 @@
 package com.api.rest.conveniencestore.dto;
 
 import com.api.rest.conveniencestore.enums.Category;
+import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
@@ -8,6 +9,8 @@ import java.time.LocalDate;
 
 public record ProductDto(
 
+
+        @Column(nullable = false, unique = true)
         @NotBlank(message = "Product name cannot be blank")
         String name,
 
@@ -16,6 +19,7 @@ public record ProductDto(
         @NotNull(message = "Price cannot be null")
         double price,
 
+        @Column(nullable = false, name = "stock_quantity")
         @NotNull(message = "Stock quantity cannot be null")
         int stockQuantity,
 
