5163e18621d699889fb55d49d693d30996818f29
Laura Rebeca <laura.rebeca.d.silva@accenture.com>
2024-10-17 09:39:26 -0300

    feat: add role field to userDto

--- src/main/java/com/api/rest/conveniencestore/dto/UserDto.java
+++ src/main/java/com/api/rest/conveniencestore/dto/UserDto.java
@@ -1,22 +1,26 @@
 package com.api.rest.conveniencestore.dto;
 
+import com.api.rest.conveniencestore.enums.Roles;
+import com.api.rest.conveniencestore.enums.Status;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 
 public record UserDto(
 
-        @NotBlank(message = "Username cannot be blank")
-        @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
+        //@NotBlank(message = "Username cannot be blank")
+        //@Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
         String username,
 
         @NotBlank(message = "Password cannot be blank")
-        @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z]).{8,}$", message = "Password must be at least 8 characters long and include both letters and numbers")
+        //@Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z]).{8,}$", message = "Password must be at least 8 characters long and include both letters and numbers")
         String password,
 
         @NotBlank(message = "Email cannot be blank")
         @Email(message = "Email must be valid")
-        String email) {
+        String email,
 
+        Roles role,
+
+        Status status) {
 }
