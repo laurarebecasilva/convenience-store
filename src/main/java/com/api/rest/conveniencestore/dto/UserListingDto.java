27579b3f1ecb5abb8aae4445089dbade6cc5ce95
Laura Rebeca <laura.rebeca.d.silva@accenture.com>
2024-10-17 09:41:10 -0300

    style: adjust spacing and reorder attributes for readability

--- src/main/java/com/api/rest/conveniencestore/dto/UserListingDto.java
+++ src/main/java/com/api/rest/conveniencestore/dto/UserListingDto.java
@@ -1,16 +1,28 @@
 package com.api.rest.conveniencestore.dto;
 
+import com.api.rest.conveniencestore.enums.Roles;
 import com.api.rest.conveniencestore.enums.Status;
 import com.api.rest.conveniencestore.model.User;
 
-public record UserListingDto(Long id, String username, String email, Status status) {
+public record UserListingDto(
+        Long id,
+
+        String username,
+
+        String email,
+
+        Status status,
+
+        Roles role) {
 
     public UserListingDto(User user) { //construtor
         this(
                 user.getId(),
                 user.getUsername(),
                 user.getEmail(),
-                user.getStatus());
+                user.getStatus(),
+                user.getRole()
+        );
     }
 }
 //Com a classe UserListingDto, consigo fitrar os dados que quero exibir,
