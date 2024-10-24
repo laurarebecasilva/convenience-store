e1a75dbc64c2b8326262ba9d49ae59014a5b250a
Laura Rebeca <laura.rebeca.d.silva@accenture.com>
2024-10-23 23:29:05 -0300

    feat: add MessageConstants class for managing application messages

---
+++ src/main/java/com/api/rest/conveniencestore/utils/MessageConstants.java
@@ -0,0 +1,24 @@
+package com.api.rest.conveniencestore.utils;
+
+public class MessageConstants {
+
+    public static final String USERNAME_NOT_FOUND = "Username not found";
+    public static final String CREDENTIALS_INVALID = "Credenciais: Username ou password invalidos.";
+    public static final String PRODUCT_ALREADY_EXISTS = "Produto já cadastrado com o nome: ";
+    public static final String NO_PRODUCTS_FOUND = "Nenhum produto cadastrado foi encontrado.";
+    public static final String INVALID_STATUS = "Status inválido: ";
+    public static final String NO_USERS_FOUND ="Nenhum usuário cadastrado foi encontrado.";
+    public static final String STATUS_INACTIVE = "O status deve ser alterado apenas para INACTIVE.";
+    public static final String PRODUCT_NOT_FOUND = "Produto com ID: %d não foi encontrado.";
+    public static final String USER_NOT_FOUND = "User with id %d not found";
+    public static final String PAYMENT_METHOD_EMPTY = "O método de pagamento não pode ser vazio.";
+    public static final String INVALID_PAYMENT_METHOD = "Método de pagamento inválido: ";
+    public static final String INVALID_ROLE = "Role inválido: ";
+    public static final String ROLE_ADMIN = "A role só pode ser alterada para ADMIN.";
+    public static final String INVALID_PRODUCT = "Produto inativo: ";
+    public static final String EMAIL_ALREADY_REGISTERED = "Email já cadastrado: ";
+    public static final String ERROR_JWT_TOKEN = "Error generating the JWT token";
+    public static final String STATUS_CANCELLED = "O status deve ser alterado apenas para CANCELLED.";
+    public static final String SALE_NOT_FOUND = "Compra de ID: %d não foi encontrada.";
+    public static final String CPF_ALREADY_EXISTS = "CPF já está registrado: ";
+}
