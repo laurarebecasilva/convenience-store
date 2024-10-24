970e5b24ff8d760834db591048845e268e9c616c
Laura Rebeca <laura.rebeca.d.silva@accenture.com>
2024-10-23 23:19:35 -0300

    feat: add ClientAlreadyExistsException for handling duplicate client registration

---
+++ src/main/java/com/api/rest/conveniencestore/exceptions/ClientAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.api.rest.conveniencestore.exceptions;
+
+public class ClientAlreadyExistsException extends Exception {
+    public ClientAlreadyExistsException(String message) {
+        super(message);
+    }
+}
