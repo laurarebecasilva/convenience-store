628a9c0e2287d9feb42737a0d226b4beead6a074
Laura Rebeca <laura.rebeca.d.silva@accenture.com>
2024-10-23 23:40:44 -0300

    feat: add UsernameValidateException for user validation

---
+++ src/main/java/com/api/rest/conveniencestore/exceptions/UsernameValidateException.java
@@ -0,0 +1,7 @@
+package com.api.rest.conveniencestore.exceptions;
+
+public class UsernameValidateException extends Exception {
+    public UsernameValidateException(String message) {
+        super(message);
+    }
+}
