6e4c3316c143b002ceffe81769436ec58581ab64
Laura Rebeca <laura.rebeca.d.silva@accenture.com>
2024-10-23 23:23:16 -0300

    refactor: add validation to client registration method

--- src/main/java/com/api/rest/conveniencestore/controller/ClientController.java
+++ src/main/java/com/api/rest/conveniencestore/controller/ClientController.java
@@ -1,8 +1,14 @@
 package com.api.rest.conveniencestore.controller;
 
 import com.api.rest.conveniencestore.dto.ClientDto;
+import com.api.rest.conveniencestore.exceptions.CpfValidateException;
+import com.api.rest.conveniencestore.exceptions.NameValidateException;
 import com.api.rest.conveniencestore.model.Client;
+import com.api.rest.conveniencestore.repository.ClientRepository;
 import com.api.rest.conveniencestore.service.ClientService;
+import com.api.rest.conveniencestore.utils.MessageConstants;
+import com.api.rest.conveniencestore.validations.CpfValidator;
+import com.api.rest.conveniencestore.validations.UserValidator;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,10 +26,28 @@ public class ClientController {
     @Autowired
     ClientService clientService;
 
+    @Autowired
+    ClientRepository clientRepository;
+
+    @Autowired
+    CpfValidator cpfValidator;
+
+    @Autowired
+    UserValidator userValidator;
+
     @PostMapping
     @Transactional
-    public ResponseEntity<Client> register(@Valid @RequestBody ClientDto clientDto) {
+    public ResponseEntity<Client> register(@Valid @RequestBody ClientDto clientDto) throws CpfValidateException, NameValidateException {
+
+        userValidator.validateNameClient(clientDto.name());
+
+        if (clientRepository.existsByCpf(clientDto.cpf())) {
+            throw new CpfValidateException(MessageConstants.CPF_ALREADY_EXISTS + clientDto.cpf());
+        }
+        cpfValidator.validateCpf(clientDto.cpf());
+
         Client savedClient = clientService.registerClient(clientDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedClient);
     }
 }
+
