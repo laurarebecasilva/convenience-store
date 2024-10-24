a57d3c704c21acab3abb774c616dde9ac463c4ef
Laura Rebeca <laura.rebeca.d.silva@accenture.com>
2024-10-23 23:26:12 -0300

    feat: add CpfValidateException for handling CPF validation errors

---
+++ src/main/java/com/api/rest/conveniencestore/exceptions/CpfValidateException.java
@@ -0,0 +1,7 @@
+package com.api.rest.conveniencestore.exceptions;
+
+public class CpfValidateException extends Exception {
+    public CpfValidateException(String message) {
+        super(message);
+    }
+}
