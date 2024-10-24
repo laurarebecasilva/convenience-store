719738decd2ddbe13de3366972603e5c4e8c1637
Laura Rebeca <laura.rebeca.d.silva@accenture.com>
2024-10-23 23:30:07 -0300

    feat: add PasswordValidateException for handling password validation errors

---
+++ src/main/java/com/api/rest/conveniencestore/exceptions/PasswordValidateException.java
@@ -0,0 +1,7 @@
+package com.api.rest.conveniencestore.exceptions;
+
+public class PasswordValidateException extends Exception {
+    public PasswordValidateException(String message) {
+        super(message);
+    }
+}
