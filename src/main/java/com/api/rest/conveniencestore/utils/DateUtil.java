9a20143762e3565c36a1e8f9efacbebaa651fe10
Laura Rebeca <laura.rebeca.d.silva@accenture.com>
2024-10-29 14:10:13 -0300

    feat: add formateDateCsv method to format LocalDateTime with DATE_FORMAT_CSV

--- src/main/java/com/api/rest/conveniencestore/utils/DateUtil.java
+++ src/main/java/com/api/rest/conveniencestore/utils/DateUtil.java
@@ -8,9 +8,14 @@ import java.time.format.DateTimeFormatter;
 @Component
 public class DateUtil {
     private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+    public static final DateTimeFormatter DATE_FORMAT_CSV = DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss");
 
     public static String formatDate(LocalDateTime dateTime) { //args local dtetime
         return dateTime.format(DATE_FORMAT);
+    }
+
+    public static String formateDateCsv(LocalDateTime dateTime) {
+        return dateTime.format(DATE_FORMAT_CSV);
     }
 }
 
